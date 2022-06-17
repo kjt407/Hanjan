7f8b5fc5b88cc0708530e8e25f75e3d20446b8a2
jongtk <kjt40700@gmail.com>
2022-06-17 11:10:35 +0900

    project init

---
+++ src/main/java/com/jongtk/hanjan/HanjanApplication.java
@@ -0,0 +1,13 @@
+package com.jongtk.hanjan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanjanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanjanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jongtk/hanjan/HanjanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jongtk.hanjan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanjanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
