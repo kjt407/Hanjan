d23e4c8c91bbbab71e83820eb910db97fce98c82
jongtk <kjt40700@gmail.com>
2022-06-19 01:45:31 +0900

    edit controller

--- src/test/java/com/jongtk/hanjan/controller/TestController.java
+++ src/test/java/com/jongtk/hanjan/controller/TestController.java
@@ -2,6 +2,7 @@ package com.jongtk.hanjan.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -9,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/test/")
 public class TestController {
 	
 	@GetMapping("get")
