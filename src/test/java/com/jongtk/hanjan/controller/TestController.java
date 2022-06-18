5ac20e2f03997e7dac162ebb6fa1692c5109c679
jongtk <kjt40700@gmail.com>
2022-06-19 01:42:05 +0900

    test jenkins build docker container /test/*

--- src/test/java/com/jongtk/hanjan/controller/TestController.java
+++ src/test/java/com/jongtk/hanjan/controller/TestController.java
@@ -3,6 +3,8 @@ package com.jongtk.hanjan.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +12,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test/")
 public class TestController {
 	
-	@GetMapping("")
+	@GetMapping("get")
 	public ResponseEntity<String> get(){
+		return new ResponseEntity<>("정상적 반환",HttpStatus.OK);
+	}
+	
+	@PostMapping("post")
+	public ResponseEntity<String> post(){
+		return new ResponseEntity<>("정상적 반환",HttpStatus.OK);
+	}
+	
+	@PutMapping("put")
+	public ResponseEntity<String> put(){
+		return new ResponseEntity<>("정상적 반환",HttpStatus.OK);
+	}
+	@DeleteMapping("delete")
+	public ResponseEntity<String> delete(){
 		return new ResponseEntity<>("정상적 반환",HttpStatus.OK);
 	}
 }
